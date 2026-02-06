93c4c20d00f7ecfbff0e0af3d35214260f6648a7
leeyewon511 <leeyewon511@gmail.com>
2026-02-06 20:55:23 +0900

    feat : setting

--- src/routes/pages/Home/HomeContainer.js
+++ src/routes/pages/Home/HomeContainer.js
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import HomePresenter from "./HomePresenter";
 
+
 const HomeContainer = () => {
-  return <HomePresenter />;
-};
+   const [list, setList] = useState([
+    {
+      id: 0,
+      imgs: "https://img.kr.gcp-karroter.net/origin/article/202511/e04184f806ae9ef169f0d15ebe70e7457026ce260e628bb3c632531f243f4b79_0.webp?f=webp&q=95&s=720x720&t=inside",
+      title: "휩드 디스커버리 키트",
+      price: "180,000",
+      area: "잠실동",
+      days: "17일전",
+    },
+    {
+      id: 1,
+      imgs: "https://img.kr.gcp-karroter.net/origin/article/202602/1124b736d617d18984c1b3c441443b284132be36c0b174fe048c795dcf3fd521_0.webp?f=webp&q=95&s=720x720&t=inside",
+      title: "스타벅스",
+      price: "80,000",
+      area: "우동",
+      days: "2일전",
+    },
+    {
+      id: 2,
+      imgs: "https://i.pinimg.com/236x/e6/86/1e/e6861e0a7be7c41a968054cc1b8d32fa.jpg",
+      title: "상의 티셔츠",
+      price: "50,000",
+      area: "송정동",
+      days: "1일전",
+    },
+  ]);
+
+  const [page, setPage]=useState([{
+    imgs1: "https://img.kr.gcp-karroter.net/origin/article/202511/e04184f806ae9ef169f0d15ebe70e7457026ce260e628bb3c632531f243f4b79_0.webp?f=webp&q=95&s=720x720&t=inside",
+    profile: "https://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg",
+    names: "녹차굿",
+    area: "서울시 송파구 잠실동",   
+    title:"휩드 디스커버리 키트",
+    category:"생활가전",
+    days:"10일전",
+    price:"180,000",
+    contents:"새제품으로 드립니다.세인트스코트 블랙 토트백과 미니백 세트입니다. 가볍고 실용적인 디자인으로 데일리백으로 좋아요. 미니백은 탈부착 가능합니다. 깔끔한 디자인이라 데일리로 들기 좋아요 ",
+    msg:1,
+    heart:4,
+    read:86,
+    goapp:"당근 앱에서 보기"
+  }]);
+
+
+  return <HomePresenter product={list} detailedPage={page} />;
+}
 
 export default HomeContainer;
--- src/routes/pages/Home/HomePresenter.jsx
+++ src/routes/pages/Home/HomePresenter.jsx
@@ -1,7 +1,78 @@
 import React from "react";
+import "./Home.css"
 
-const HomePresenter = () => {
-  return <div>Welcome Home</div>;
+const HomePresenter = (props) => {
+  const box1 = props.product[0];
+  const box2 = props.product[1];
+  const box3 = props.product[2];
+  const details = props.detailedPage[0];
+
+  return (
+    <div>
+    <div className="Box">
+      <article className="Box1" >
+        <img src={box1.imgs} className="img1" />
+        <p className="title">{box1.title}</p>
+        <p className="price">{box1.price}</p>
+        <div className="info" >
+        <p className="area">{box1.area}</p>
+        <p className="days">{box1.days}</p> 
+        </div>
+      </article>
+
+      <article className="Box2"> 
+        <img src={box2.imgs} className="img2" />
+        <p className="title">{box2.title}</p>
+        <p className="price">{box2.price}</p>
+        <div className="info">
+        <p className="area">{box2.area}</p>
+        <p className="days">{box2.days}</p>
+        </div>
+      </article>
+
+      <article className="Box3"> 
+        <img src={box3.imgs} className="img3" />
+        <p className="title">{box3.title}</p>
+        <p className="price">{box3.price}</p>
+        <div className="info">
+        <p className="area">{box3.area}</p>
+        <p className="days">{box3.days}</p>
+        </div>
+      </article>
+    </div>
+
+    <div className="details">
+      <div className="content1">
+        <img src={details.imgs1} className="img4"/>
+
+       <div className="info1">
+        <img src={details.profile} className="profile"/>
+        <div className="info1_1">
+        <p className="names">{details.names}</p>
+        <p className="area">{details.area}</p>
+        </div>
+       </div>
+      </div>
+
+      <div className="content2">
+        <h1 className="title1">{details.title}</h1>
+        <h2 className="meta-data" >
+        <p className="category">{details.category}</p>
+        <p className="days1">{details.days}</p>
+        </h2>
+        <h3 className="price">{details.price}원</h3>
+        <p className="contents">{details.contents}</p>
+        <div className="reactions">
+        <span className="msg">채팅{details.msg}</span>
+        <span className="heart">관심{details.heart}</span>
+        <span className="read">조회{details.read}</span>
+        </div>
+        <div className="goapp">{details.goapp}</div>
+      </div>
+    </div>
+    </div>
+  );
 };
+
 
 export default HomePresenter;
